7c496241739a420ed9888baff34cb8869f2e1b4a
Roman Chamara <romacamara6@gmail.com>
2026-07-12 19:22:21 +0300

    feature: add file cache for video subtitles and simple_search return all time-codes by word

--- main.go
+++ main.go
@@ -2,36 +2,71 @@ package main
 
 import (
 	"fmt"
-	"github.com/charm1092/YT_video_search/internal/subtitles"
+	"net/url"
+	"os"
+	"path/filepath"
+
 	"github.com/charm1092/YT_video_search/internal/search"
+	"github.com/charm1092/YT_video_search/internal/subtitles"
 )
 
-func main()  {
-	url := "https://www.youtube.com/watch?v=f2kvTd54uMw"
+func main() {
+	urlRaw := "https://www.youtube.com/watch?v=f2kvTd54uMw"
+	parsedURL, err := url.Parse(urlRaw)
+	if err != nil {
+		fmt.Println("некорректная ссылка")
+		return
+	}
 
-	path_json, err := subtitles.Fetcher(url)
-	if err != nil {
-		fmt.Println("error: ", err)
+	videoID := parsedURL.Query().Get("v")
+	if videoID == "" {
+		fmt.Println("в ссылке нет ID видео")
 		return
 	}
-	
-	segments, err := subtitles.ParseFile(path_json)
-	if err != nil {
-		fmt.Println("error: ", err)
+
+	cacheDir := filepath.Join("data", "video", videoID)
+	if err := os.MkdirAll(cacheDir, 0755); err != nil {
+		fmt.Println("error:", err)
 		return
 	}
-	
-	if err := subtitles.SaveSegments("subs.txt", segments); err != nil {
-		fmt.Println("error: ", err)
+
+	jsonBasePath := filepath.Join(cacheDir, "subtitles")
+	jsonPath := jsonBasePath + ".ru.json3"
+	segmentsPath := filepath.Join(cacheDir, "subs.txt")
+
+	if _, err := os.Stat(segmentsPath); os.IsNotExist(err) {
+		if _, err := os.Stat(jsonPath); os.IsNotExist(err) {
+			jsonPath, err = subtitles.Fetcher(urlRaw, jsonBasePath)
+			if err != nil {
+				fmt.Println("error:", err)
+				return
+			}
+		} else if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		segments, err := subtitles.ParseFile(jsonPath)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		if err := subtitles.SaveSegments(segmentsPath, segments); err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+	} else if err != nil {
+		fmt.Println("error:", err)
 		return
 	}
 
 	var s string
 	fmt.Scan(&s)
 
-	answer, err := search.FindWordV1("subs.txt", s)
+	answer, err := search.FindWordV2(segmentsPath, s)
 	if err != nil {
-		fmt.Println("error: ", err)
+		fmt.Println("error:", err)
 		return
 	}
 
